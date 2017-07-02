e1ddc58eb6814a9a185225776357e00944e248ec
Rocco John Mondo <jmondo@att.net>
2017-07-02 10:08:47 -0400

    Updated Front End

---
+++ server.js
@@ -0,0 +1,10 @@
+// Dependencies for Week 18 Project - (To Get full credit... of which I probably won't... because I'm a jackass)
+
+var express = require("express");
+var exphbs = require("express-handlebars");
+var mongoose = require("mongoose");
+var bodyParser = require("body-parser");
+
+
+var request = require("request");
+var cheerio = require("cheerio");
